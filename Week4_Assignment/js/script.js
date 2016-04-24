39963d88ac95421d9aa25e06b1704d67098593d8
honeyanant <honeyanant@gmail.com>
2016-04-24 09:51:16 +0530

    2nd commit

--- Week4_Assignment/js/script.js
+++ Week4_Assignment/js/script.js
@@ -83,10 +83,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl, 
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  function buildAndShowHomeHTML(responseText){
+	  document.querySelector("#main-content")
+      .innerHTML = responseText;
+  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
+
 
 
 // Builds HTML for the home page based on categories array
